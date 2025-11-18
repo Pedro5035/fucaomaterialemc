e0fbf5fa86ef45423a3b5e004d52bf32518155ad
Pedro5035 <pedrox503522@gmail.com>
2025-11-18 19:47:35 -0300

    Create fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,12 @@
+int fatorial(int n) {
+    if (n < 0) {
+        return -1; // erro para número negativo
+    }
+
+    int resultado = 1;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+
+    return resultado;
+}
